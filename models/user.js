76e7d60efe369ed282861f844377929afdbb2da7
Vik63 <sfoto116@yandex.ru>
2022-06-21 20:14:53 +0400

    3-14

--- models/user.js
+++ models/user.js
@@ -5,14 +5,10 @@ const isEmail = require('validator/lib/isEmail');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    minlength: 2,
-    maxlength: 30,
     default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    minlength: 2,
-    maxlength: 30,
     default: 'Исследователь',
   },
   avatar: {
